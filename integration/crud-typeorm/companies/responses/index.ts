cb15f00885fc4fa2ebf94c30f82b4308a20ea488
Daniil Belov <daniil.belov07@gmail.com>
2021-09-05 17:16:51 +0100

    feat: clean

--- integration/crud-typeorm/companies/responses/index.ts
+++
@@ -1,6 +0,0 @@
-import { SerializeOptions } from '@nestjsx/crud';
-import { GetCompanyResponseDto } from './get-company-response.dto';
-
-export const serialize: SerializeOptions = {
-  get: GetCompanyResponseDto,
-};
--- integration/crud-typeorm/devices/response/index.ts
+++
@@ -1,6 +0,0 @@
-import { SerializeOptions } from '@nestjsx/crud';
-import { DeleteDeviceResponseDto } from './delete-device-response.dto';
-
-export const serialize: SerializeOptions = {
-  delete: DeleteDeviceResponseDto,
-};
